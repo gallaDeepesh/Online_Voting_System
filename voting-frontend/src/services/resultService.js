ccba80619dac4d596514e2e30af76a2a39f8bde3
gallaDeepesh <galladeepesh@gmail.com>
2026-06-23 16:19:06 +0530

    updated api end point ec2 instance

--- voting-frontend/src/services/resultService.js
+++ voting-frontend/src/services/resultService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/results";
+const API_URL = `${import.meta.env.VITE_API_URL}/results `;
 
 // Helper function to dynamically generate authorization headers
 const getAuthHeaders = () => {
