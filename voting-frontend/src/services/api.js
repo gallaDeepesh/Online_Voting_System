9c3b1ea1b6eeb39afe6977edc06abf33872bf66d
gallaDeepesh <galladeepesh@gmail.com>
2026-06-23 16:19:06 +0530

    updated api end point ec2 instance

--- voting-frontend/src/services/api.js
+++ voting-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth";
+const API_URL = `${import.meta.env.VITE_API_URL}/auth`
 export const registerUser = (userData) => {
     return axios.post(`${API_URL}/register`, userData);
 };
